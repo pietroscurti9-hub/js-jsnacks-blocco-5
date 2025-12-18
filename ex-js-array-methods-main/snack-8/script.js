7a71154d18b5ac012e58b656ce21aa9875119cbc
Pietro Scurti <pietroscurti9@gmail.com>
2025-12-18 17:00:21 +0100

    snack 8 spaziatura corretta

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -22,7 +22,7 @@ const students = [
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
 
-const marco = students.find(indice =>{
+const marco = students.find(indice => {
   return indice.name === "Marco Lanci"
 });
 
