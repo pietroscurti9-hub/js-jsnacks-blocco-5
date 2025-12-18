acdc2d5c582174bd3a9d0a0ea744f65722a21fb7
Pietro Scurti <pietroscurti9@gmail.com>
2025-12-18 16:31:46 +0100

    primi esercizi

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,10 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+
+names.forEach(nome => {
+  console.log(nome);
+});
---
+++ ex-js-array-methods-main/snack-2/script.js
@@ -0,0 +1,12 @@
+const people = [
+  { name: 'Paolo', age: 35 },
+  { name: 'Giulia', age: 24 },
+  { name: 'Marco', age: 67 }
+];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Paolo', 'Giulia', 'Marco'
+
+people.forEach((people) => {
+  console.log(`${people.name}`)
+})
